fe91a5ff69534c50479324ef539dbe90205cb069
kaimbakarimu-a11y <kaimbakarimu@gmail.com>
2026-02-02 00:54:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process', 
-            '--disable-gpu'
-        ],
-        executablePath: process.env.CHROME_PATH || '/usr/bin/google-chrome'
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/usr/bin/google-chrome'
     }
 });
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+    console.log('QR CODE IMEFIKA! Scan sasa hivi:');
+});
+
+client.on('ready', () => {
+    console.log('Boti imewaka! Client is ready!');
+});
+
+client.on('message', async (msg) => {
+    if (msg.body === '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
