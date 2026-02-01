6e249a2d945d1655e80f1222ee83a9a2430aa3ba
kaimbakarimu-a11y <kaimbakarimu@gmail.com>
2026-02-02 00:14:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-
 const client = new Client({
-    authStrategy: new LocalAuth()
-});
-
-const myEmojis = ['🥰', '😍', '❤️‍🔥', '💥', '💘', '💝', '❤️', '💞', '💯', '💜', '🔥'];
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-    console.log('--- SCAN QR CODE MPYA HAPA ---');
-});
-
-client.on('ready', () => {
-    console.log('StatusTurboBot: IKO ONLINE SASA!');
-});
-
-client.on('message_create', async (msg) => {
-    if (msg.from === 'status@broadcast') {
-        try {
-            // View status
-            await client.sendSeen(msg.from);
-            
-            // Like na Random Emoji
-            const pickEmoji = myEmojis[Math.floor(Math.random() * myEmojis.length)];
-            await msg.react(pickEmoji);
-            
-            console.log('Turbo Action: Viewed & Reacted!');
-        } catch (err) {
-            // Skip errors
-        }
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', 
+            '--disable-gpu'
+        ],
+        executablePath: process.env.CHROME_PATH || '/usr/bin/google-chrome'
     }
 });
-
-client.initialize();
